d382ea459837936bdfa40d9d5cf9cda723850428
Walter Tronics <waltertronics02@gmail.com>
2022-08-10 10:59:35 +0100

    Creating pick and store functions

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ $(function() {
 let squaresCount = 6,
 
 //Initializing a color array
-colors = [],
+colorArr = [],
 
 //Selecting the squares
 squares = document.querySelectorAll(".square");
 
 //Appending a color from the generarated colors to the display output
-$('#colorId').text(pickColor());
+//$('#colorId').text(pickColor());
 
 
 //Function for changing the background color of the squares
@@ -26,14 +26,34 @@ function randomColor() {
     let r = Math.floor(Math.random() * 256);
     let g = Math.floor(Math.random() * 256);
     let b = Math.floor(Math.random() * 256);
-    return "rgb(" + r + ", " + g + ", " + b + ")";
+    //concatinating the rgb values to form a color
+    return "RGB(" + r + ", " + g + ", " + b + ")";
 }
 
 
 //Function to store random color in an array
 function storeGeneratedColors() {
+
+    //Looping through the squaresCount and pushing the random colors to the array
     for (let i = 0; i < squaresCount; i++) {
-        colors.push(randomColor());
+        //Push the random color to the array
+        colorArr.push(randomColor());
     }
 }
+
+
+//Function to pick a random color from the array
+function pickColor() {
+
+    //Generate a random number
+    let randomIndex = Math.floor(Math.random() * colorArr.length);
+
+    //Return the color at the random index
+    return colorArr[randomIndex];
+}
+
+storeGeneratedColors();
+pickColor();
+console.log(colorArr);
+console.log(pickColor());
 });
