f2da7f69537ed0eab714ee0f3906f7b48286013f
Walter Tronics <waltertronics02@gmail.com>
2022-08-10 13:15:45 +0100

    Created the game logic and reset function

--- index.js
+++ index.js
@@ -3,14 +3,21 @@ $(function() {
 //Selecting the elements and inintializing variables
 let squaresCount = 6,
 
-//Initializing a color array
-colorArr = [],
+//Assigning the stored generated function to a variable
+colorVar = storeGeneratedColors(),
+
+//Initializing a picked color
+pickedColor = pickColor(),
+
+//Create a score variable
+score = 0,
 
 //Selecting the squares
 squares = document.querySelectorAll(".square");
 
 //Appending a color from the generarated colors to the display output
-//$('#colorId').text(pickColor());
+$('#colorId').text((pickedColor).toUpperCase());
+
 
 
 //Function for changing the background color of the squares
@@ -20,6 +27,16 @@ function changeColor(color) {
     }
 }
 
+//Function to pick a random color from the array
+function pickColor() {
+
+    //Generate a random number
+    let randomIndex = Math.floor(Math.random() * colorVar.length);
+
+    //Return the color at the random index
+    return colorVar[randomIndex];
+}
+
 
 //Function to generate random colors
 function randomColor() {
@@ -27,33 +44,72 @@ function randomColor() {
     let g = Math.floor(Math.random() * 256);
     let b = Math.floor(Math.random() * 256);
     //concatinating the rgb values to form a color
-    return "RGB(" + r + ", " + g + ", " + b + ")";
+    return "rgb(" + r + ", " + g + ", " + b + ")";
 }
 
 
 //Function to store random color in an array
 function storeGeneratedColors() {
 
+    //Initializing a color array
+    let colorArr = [];
+
     //Looping through the squaresCount and pushing the random colors to the array
     for (let i = 0; i < squaresCount; i++) {
         //Push the random color to the array
         colorArr.push(randomColor());
     }
+    //Return the array
+    return colorArr;
+}
+
+//The game logic
+for (let i = 0; i < squares.length; i++) {
+    //Add the stored colors to the squares
+    squares[i].style.background = colorVar[i];
+
+    //Add event listeners to the squares
+    squares[i].addEventListener("click", function() {
+        //Get the color of the clicked square
+        let clickedColor = this.style.background;
+        //Check if the clicked color is equal to the picked color
+        if (clickedColor === pickedColor) {
+            //Change the background color of the header to the picked color
+            $('header').css('background', clickedColor);
+            //Change the border color of the score container to the picked color
+            $('#score').css('borderColor', clickedColor);
+            //Change the background color of all the squares to the picked color
+            changeColor(pickedColor);
+            //increment the score by 1
+            score++;
+            //Update the score in the score display
+            $('#score').text(score);
+        } else {
+            $(this).css('background-color', '#232222');
+        }
+        console.log(clickedColor);
+    });
+}
+
+function reset() {
+    //Generate new colors
+    colorVar = storeGeneratedColors();
+    //Pick a new random color from the array
+    pickedColor = pickColor();
+    //Change the colorDisplay to match the picked color
+    $('#colorId').text((pickedColor).toUpperCase());
+    //Change the color of the squares randomly again
+    for (let i = 0; i < squares.length; i++) {
+        squares[i].style.background = colorVar[i];
+    }
+    //Change the header background color to the default color
+    $('header').css('background-color', 'RGB(14, 144, 154)');
+    //Change the border color of the score container to the default color
+    $('#score').css('borderColor', 'RGB(14, 144, 154)');
 }
 
 
-//Function to pick a random color from the array
-function pickColor() {
 
-    //Generate a random number
-    let randomIndex = Math.floor(Math.random() * colorArr.length);
-
-    //Return the color at the random index
-    return colorArr[randomIndex];
-}
-
-storeGeneratedColors();
-pickColor();
-console.log(colorArr);
-console.log(pickColor());
+console.log(colorVar); 
+console.log(pickedColor);
 });
