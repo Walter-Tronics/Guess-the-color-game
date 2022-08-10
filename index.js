4ce34593a20123753fd8eb54b592f8cf8e0e64ae
Walter Tronics <waltertronics02@gmail.com>
2022-08-10 10:08:41 +0100

    Initiating calls

---
+++ index.js
@@ -0,0 +1,13 @@
+$(function() {
+
+//Selecting the elements and inintializing variables
+let squaresCount = 6,
+colors = [],
+squares = document.querySelectorAll(".square");
+
+//Appending a color from the generarated colors to the display output
+$('#colorId').text(pickColor());
+
+
+
+});
